008af8c3bd3ed89e7410a15dfea0eaafb8571498
marville001 <mwangimartin1904@gmail.com>
2021-08-31 15:59:25 +0300

    Added portfolio heading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.scss";
 import { Switch, Route } from "react-router-dom";
 // import AboutPage from "./Pages/AboutPage";
-import PortfliosPage from "./Pages/PortfoliosPage";
 // import BlogsPage from "./Pages/BlogsPage";
 // import ContactPage from "./Pages/ContactPage";
 import Header from "./Components/Header";
 import Home from "./Pages/Home";
 import Footer from "./Components/Footer";
+import Portfolios from "./Pages/Portfolio";
 
 function App() {
   
@@ -20,9 +20,7 @@ function App() {
               <Home />
             </Route>
              
-            <Route path="/portfolios" exact>
-              <PortfliosPage />
-            </Route>
+            <Route path="/portfolios" exact component={Portfolios}/>
             {/*<Route path="/blogs" exact>
               <BlogsPage />
             </Route>
--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 // import Categories from "../Components/Categories";
 // import MenuItems from "../Components/MenuItems";
-import Tittle from "../Components/Tittle";
 import portfolios from "../Components/allportfolios";
 import { useState, useEffect } from "react";
 
@@ -15,17 +14,17 @@ const allCategories = [
   "Postgress",
 ];
 
-function PortfoliosPage() {
+const Portfolios =()=> {
   const [categories, setCategories] = useState([]);
   const [menuItems, setMenuItems] = useState([]);
 
-  const filter = (name) => {
-    const new_portfolios = portfolios.filter((portfolio) =>
-      portfolio.category.includes(name)
-    );
-    if (name === "All") setMenuItems(portfolios);
-    else setMenuItems(new_portfolios);
-  };
+  // const filter = (name) => {
+  //   const new_portfolios = portfolios.filter((portfolio) =>
+  //     portfolio.category.includes(name)
+  //   );
+  //   if (name === "All") setMenuItems(portfolios);
+  //   else setMenuItems(new_portfolios);
+  // };
 
   useEffect(() => {
     setMenuItems(portfolios);
@@ -33,9 +32,9 @@ function PortfoliosPage() {
   }, []);
 
   return (
-    <div className="PortfolioPage">
+    <div className="portfolios container">
       <div className="title">
-        <Tittle title={"Portfolios"} span={"portfolios"} />
+        <h1>My Portfolios</h1>
       </div>
       {/* <div className="portfolios-data">
         <Categories filter={filter} categories={categories} />
@@ -45,4 +44,4 @@ function PortfoliosPage() {
   );
 }
 
-export default PortfoliosPage;
+export default Portfolios;
