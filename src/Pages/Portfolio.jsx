b935e2709dfce183ffa3543cb6bd6255e8c3681b
marville001 <mwangimartin1904@gmail.com>
2021-09-03 23:26:49 +0300

    Animated title

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import PortfoliosSection from "../Components/PortfoliosComponents/PortfoliosSection";
+import { motion } from "framer-motion";
 
+const titleVariants = {
+  hidden: {
+    y: -50
+  },
+  visible: {
+    y: [5, -30, 5, 0],
+    transition:{
+      type:"spring",
+      stiffness: 300
+    }
+  },
+};
 const Portfolios = () => {
   return (
     <div className="portfolios container">
-      <div className="title">
+      <motion.div className="title"
+      variants = {titleVariants}
+      initial="hidden"
+      animate="visible"
+      >
         <h1>Projects I have done</h1>
-      </div>
+      </motion.div>
       <PortfoliosSection />
     </div>
   );
