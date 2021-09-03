e2563e853cb5b29c4322d3414e0373b9ba807c50
marville001 <mwangimartin1904@gmail.com>
2021-09-03 23:27:06 +0300

    Updates

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -4,23 +4,24 @@ import { motion } from "framer-motion";
 
 const titleVariants = {
   hidden: {
-    y: -50
+    y: -50,
   },
   visible: {
     y: [5, -30, 5, 0],
-    transition:{
-      type:"spring",
-      stiffness: 300
-    }
+    transition: {
+      type: "spring",
+      stiffness: 300,
+    },
   },
 };
 const Portfolios = () => {
   return (
     <div className="portfolios container">
-      <motion.div className="title"
-      variants = {titleVariants}
-      initial="hidden"
-      animate="visible"
+      <motion.div
+        className="title"
+        variants={titleVariants}
+        initial="hidden"
+        animate="visible"
       >
         <h1>Projects I have done</h1>
       </motion.div>
