524d5110ca25fd91353d4f13b49f81b08cd75d9b
Martin Mwangi <mwangimartin1904@gmail.com>
2021-11-15 09:29:07 +0300

    projects update

--- src/Components/HomeComponents/ProjectsSection.jsx
+++ src/Components/HomeComponents/ProjectsSection.jsx
@@ -4,7 +4,12 @@ import { useInView } from "react-intersection-observer";
 import Tittle from "../Tittle";
 import ProjectCard from "../ProjectCard";
 import { Link } from "react-router-dom";
-
+import project1 from "../../img/Project1.png"
+import faceDetectorImg from "../../img/portImages/faceDetector.png"
+import covidTracker from "../../img/portImages/covidTracker.png"
+import budgetApp from "../../img/portImages/budgetApp.png"
+import agencySite from "../../img/portImages/agencySite.png"
+import ashop from "../../img/portImages/ashop.png"
 const ProjectsSection = () => {
   const animation = useAnimation();
   const { ref, inView } = useInView({ threshold: 0 });
@@ -31,9 +36,34 @@ const ProjectsSection = () => {
       >
         <Tittle title="My Projects" index="03" />
         <div className="projects-cards">
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
+          <ProjectCard
+            name={"OctoProfile"}
+            description={"A nicer look at your GitHub profile and repository stats with data visualizations of your top languages and stars. Sort through your top repos by number of stars, forks, and size."}
+            img={project1}
+            github={"https://github.com/marville001"}
+            web={"hhjhj"}
+            // bchiang7/octoprofile
+            langs={["React.Js", "Node.Js", "Express.Js","PostGresDB"]}
+          />
+
+          <ProjectCard
+            name={"Face Detector"}
+            description={"A nicer look at your GitHub profile and repository stats with data visualizations of your top languages and stars. Sort through your top repos by number of stars, forks, and size."}
+            img={faceDetectorImg}
+            github={"https://github.com/marville001/octoprofile"}
+            web={"hhjhj"}
+            langs={["React.Js", "Node.Js", "Express.Js","PostGresDB"]}
+          />
+
+          <ProjectCard
+            name={"Covid19 Tracker"}
+            description={"A nicer look at your GitHub profile and repository stats with data visualizations of your top languages and stars. Sort through your top repos by number of stars, forks, and size."}
+            img={covidTracker}
+            github={"https://github.com/marville001/octoprofile"}
+            web={"hhjhj"}
+            langs={["React.Js", "Covid19 API"]}
+          />
+          
         </div>
       </motion.div>
       <motion.div
--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
-import project1 from "../img/Project1.png"
-const ProjectCard = () => {
+
+const ProjectCard = ({name, img, description, github, web, langs}) => {
     const animation = useAnimation();
   const { ref, inView } = useInView({ threshold: 0 });
   useEffect(() => {
@@ -26,21 +26,25 @@ const ProjectCard = () => {
                 <div>
                     <p class="project-overline">Featured Project</p>
                     <h3 class="project-title">
-                        <a href="https://octoprofile.now.sh" rel="noopener noreferrer" target="_blank">OctoProfile</a>
+                        <a href="https://octoprofile.now.sh" rel="noopener noreferrer" target="_blank">{name}</a>
                     </h3>
                     <div class="project-description">
-                        <p>A nicer look at your GitHub profile and repository stats with data visualizations of your top languages and stars. Sort through your top repos by number of stars, forks, and size.</p>
+                        <p>{description}</p>
                     </div>
-                    <ul class="project-tech-list"><li>Next.js</li><li>Chart.js</li><li>GitHub API</li></ul>
+                    <ul class="project-tech-list">
+                      {langs?.map((lang, i)=>(
+                        <li>{lang}</li>
+                      ))}
+                      </ul>
                     <div class="project-links">
-                        <a href="https://github.com/bchiang7/octoprofile" aria-label="GitHub Link" rel="noopener noreferrer" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></a>
-                        <a href="https://octoprofile.now.sh" aria-label="External Link" class="external" rel="noopener noreferrer" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><title>External Link</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg></a>
+                        <a href={github} aria-label="GitHub Link" rel="noopener noreferrer" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></a>
+                        <a href={web} aria-label="External Link" class="external" rel="noopener noreferrer" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><title>External Link</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg></a>
                     </div>
                 </div>
             </div>
             <div className="project-image">
                 <a href="#sss">
-                    <img src={project1} alt="Project Name" />
+                    <img src={img} alt="Project Name" />
                 </a>
             </div>
         </motion.div>
