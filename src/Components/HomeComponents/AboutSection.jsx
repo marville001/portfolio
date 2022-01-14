d338bda1ecaced919b8c940f637929a3a5e18b76
Martin Mwangi <mwangimartin1904@gmail.com>
2022-01-14 18:03:56 +0300

    Updates

--- src/Components/HomeComponents/AboutSection.jsx
+++ src/Components/HomeComponents/AboutSection.jsx
@@ -8,7 +8,7 @@ import { useInView } from "react-intersection-observer";
 
 const AboutSection = () => {
   const animation = useAnimation();
-  const { ref, inView } = useInView({ threshold:0 });
+  const { ref, inView } = useInView({ threshold: 0 });
 
   useEffect(() => {
     if (inView) {
